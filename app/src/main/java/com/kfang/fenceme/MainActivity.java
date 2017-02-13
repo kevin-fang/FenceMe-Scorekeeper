3d06de8018e1c39c78eff5d284a2d06b142c8f9d
Kevin Fang <kfang@commschool.org>
2017-02-13 10:38:10 -0500

    implemented timer functionality

--- app/src/main/java/com/kfang/fenceme/MainActivity.java
+++ app/src/main/java/com/kfang/fenceme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kfang.fenceme;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -8,10 +9,75 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int TO_ADD = 1;
-    int TO_SUBTRACT = 0;
+    public static final int TO_ADD = 1;
+    public static final int TO_SUBTRACT = 0;
 
-    View.OnClickListener createOnClickListener(final TextView score, final int toAdd) {
+    Button addRed;
+    Button subtractRed;
+    Button addGreen;
+    long mStartTime = 0;
+    Button subtractGreen;
+    Button startTimer;
+    Button stopTimer;
+    private long mCurrentTime = 180000;
+    private TextView mCurrentTimer;
+    private Handler mHandler = new Handler();
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            mCurrentTime = (int) (mCurrentTime - 1000);
+
+            int newTime = (int) mCurrentTime / 1000;
+            int seconds = newTime % 60;
+            int minutes = newTime / 60;
+
+            if (seconds < 10 && seconds >= 0) {
+                mCurrentTimer.setText("" + minutes + ":0" + seconds);
+            } else {
+                mCurrentTimer.setText("" + minutes + ":" + seconds);
+            }
+
+            mHandler.postDelayed(this, 1000);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView redScore = (TextView) findViewById(R.id.red_score);
+        final TextView greenScore = (TextView) findViewById(R.id.green_score);
+        addRed = (Button) findViewById(R.id.plus_red);
+        subtractRed = (Button) findViewById(R.id.minus_red);
+        addGreen = (Button) findViewById(R.id.plus_green);
+        subtractGreen = (Button) findViewById(R.id.minus_green);
+
+        addRed.setOnClickListener(createOnClickListener(redScore, TO_ADD));
+        subtractRed.setOnClickListener(createOnClickListener(redScore, TO_SUBTRACT));
+        addGreen.setOnClickListener(createOnClickListener(greenScore, TO_ADD));
+        subtractGreen.setOnClickListener(createOnClickListener(greenScore, TO_SUBTRACT));
+        mCurrentTimer = (TextView) findViewById(R.id.timer);
+
+        final View.OnClickListener mStartListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                mStartTime = System.currentTimeMillis();
+                mHandler.removeCallbacks(mUpdateTimeTask);
+                mHandler.postDelayed(mUpdateTimeTask, 1000);
+            }
+        };
+        View.OnClickListener mStopListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                mHandler.removeCallbacks(mUpdateTimeTask);
+            }
+        };
+        startTimer = (Button) findViewById(R.id.start_timer);
+        startTimer.setOnClickListener(mStartListener);
+
+        stopTimer = (Button) findViewById(R.id.stop_timer);
+        stopTimer.setOnClickListener(mStopListener);
+
+    }
+
+    private View.OnClickListener createOnClickListener(final TextView score, final int toAdd) {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,31 +88,9 @@ public class MainActivity extends AppCompatActivity {
                 } else if (toAdd == TO_SUBTRACT && value > 0) {
                     value -= 1;
                 }
-                score.setText(Integer.toString(value));
+                score.setText("" + value);
             }
 
         };
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final TextView redScore = (TextView) findViewById(R.id.red_score);
-        final TextView greenScore = (TextView) findViewById(R.id.green_score);
-
-        Button addRed = (Button) findViewById(R.id.plus_red);
-        addRed.setOnClickListener(createOnClickListener(redScore, TO_ADD));
-        Button subtractRed = (Button) findViewById(R.id.minus_red);
-        subtractRed.setOnClickListener(createOnClickListener(redScore, TO_SUBTRACT));
-        Button addGreen = (Button) findViewById(R.id.plus_green);
-        addGreen.setOnClickListener(createOnClickListener(greenScore, TO_ADD));
-        Button subtractGreen = (Button) findViewById(R.id.minus_green);
-        subtractGreen.setOnClickListener(createOnClickListener(greenScore, TO_SUBTRACT));
-
-        final TextView currentTimer = (TextView) findViewById(R.id.timer);
-        Button startTimer = (Button) findViewById(R.id.start_timer);
-
-
     }
 }
